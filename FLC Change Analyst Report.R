6571d731dbc9edc02b79b5e514bac1762aefd573
Bastien Mazeran <bastien.mazeran@autodesk.com>
2017-12-07 10:45:57 -0800

    Update FLC Change Analyst Report.R

--- FLC Change Analyst Report.R
+++ FLC Change Analyst Report.R
@@ -3,43 +3,43 @@
 ## how to extract information from two workspaces using Autodesk Fusion Lifecycle V3 API.
 #########################################################################################
 # Define Fusion Lifecycle username and password, and tenant URL
-User_Name <- "Input your Autodesk ID here"
-User_Password <- "Input your Autodesk ID password here"
-Tenant_Url <- "Input your Fusion Lifecycle site Url here"
-Items_BOMs_Workspace_Id <- "Input the workspace Id of your Items & BOMs workspace here"
-Change_Orders_Workspace_Id <- "Input the workspace Id of your Change Orders workspace here"
+# User_Name <- "Input your Autodesk ID here"
+# User_Password <- "Input your Autodesk ID password here"
+# Tenant_Url <- "Input your Fusion Lifecycle site Url here"
+# Items_BOMs_Workspace_Id <- "Input the workspace Id of your Items & BOMs workspace here"
+# Change_Orders_Workspace_Id <- "Input the workspace Id of your Change Orders workspace here"
+User_Name = "bastien.mazeran"
+User_Password = "quinsac4$"
+Tenant_Url = "https://adskmazerab.autodeskplm360.net"
+Items_BOMs_Workspace_Id = "57"
+Change_Orders_Workspace_Id = "9"
+ECO_Number = "CO000012"
+ECO_DmsId = ""
 
 #Load libraries required for the R script
 library(httr)
 library(jsonlite)
 
 #Use Fusion Lifecycle Authentication REST V1 API to get access token
-App_Authenticate <- POST(Tenant_Url "/rest/auth/1/login",
-                 add_headers("Accept" = "application/json"),
-                 add_headers("Content-Type" = "application/json"),
-                 body=I(list(userID = User_Name,
-                             password = User_Password)),
-                 encode = "form")
-Access_Token <- paste("Bearer", content(App_Authenticate)$access_token,  sep=" ")
+login_body <- list(userID = User_Name, password = User_Password)
+login_body = toJSON(login_body, pretty = TRUE, auto_unbox = TRUE)
+req <- httr::POST(paste0(Tenant_Url, "/rest/auth/1/login"),
+                  httr::add_headers(
+                    "Accept" = "application/json",
+                    "Content-Type" = "application/json"
+                  ),
+                  body = login_body
+);
 
-
-
-# Clear Variables
-rm(i, User_Name,
-   User_Password,
-   Tenant_Url,
-   Items_BOMs_Workspace_Id,
-   Change_Orders_Workspace_Id
-   )
-© 2017 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-API
-Training
-Shop
-Blog
-About
+#Extract the cookie
+if (status_code(req) == '200') {
+  print(lapply(cookies(req), "[[", 2)$value)
+  search_req <- httr::GET(paste0(Tenant_Url, "/api/v3/search-results?query=", ECO_Number, "&workspace=", Change_Orders_Workspace_Id),
+                          httr::add_headers("Content-Type" = "application/json"),
+                          set_cookies("JSESSIONID" = lapply(cookies(req), "[[", 2)$value)
+  );
+  status_code(search_req)
+  content(search_req)
+  # Get list of affected items for that ECO through GET https://adskmazerab.autodeskplm360.net/api/v3/workspaces/9/items/7468/affected-items 
+  # Get BOM for each affected items through https://adskmazerab.autodeskplm360.net/api/v3/workspaces/57/items/7422/bom 
+}
